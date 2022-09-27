51e18fb5c275a711c48e7d1a9a067f1375666db1
Jyrki Heinonen <jyrki.j.heinonen@cgi.com>
2022-09-27 20:45:36 +0300

    first version

--- StatusWithDB/index.ts
+++ StatusWithDB/index.ts
@@ -7,6 +7,20 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    // if (name) {
+    //     context.bindings.outputDocument = JSON.stringify({
+    //         // create a random ID
+    //         id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+    //         name: name
+    //     });
+    // }
+
+    var mongoClient = require("mongodb").MongoClient;
+    mongoClient.connect("mongodb://cosmosdb-storm-chaos-customer-single:DEDYI8pHVQVY0Y0sF81ClYNDhHKLEkyV8PREUcgEjB91it7NcSZzRjaPCUjyRWSAmnlURkN9cGPmkIUyhOKoow==@cosmosdb-storm-chaos-customer-single.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosdb-storm-chaos-customer-single@", function (err, db) {
+        context.log('MongoDB connection successful!');
+        db.close();
+    });
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
